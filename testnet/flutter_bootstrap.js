d0aa49b87cba9e5314924ee5a441c3967d2cd1a5
ttx89-dev <ttx89.dev@gmail.com>
2025-10-17 11:51:12 +0200

    Fix account dashboard - parse accounts array correctly

--- testnet/flutter_bootstrap.js
+++ testnet/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2551554149"
+    serviceWorkerVersion: "1083223750"
   }
 });
